5d264abb525b9bd796c8967df8621bd1a491531f
Tapiiri <ilmari@tarpila.fi>
2026-04-24 00:29:48 +0300

    feat: nix develop shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "AcademicCultureEnjoyers dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+          ];
+
+          shellHook = ''
+            echo "Node $(node --version) / npm $(npm --version)"
+          '';
+        };
+      });
+}
